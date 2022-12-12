1893372b128cc3f596cfefcf67f3baab4d585cb0
tanthana48 <113759642+tanthana48@users.noreply.github.com>
2022-12-12 23:45:54 +0700

    db

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ app.config['MYSQL_HOST'] = cred['mysql_host']
 app.config['MYSQL_USER'] = cred['mysql_user']
 app.config['MYSQL_PASSWORD'] = cred['mysql_password']
 app.config['MYSQL_DB'] = cred['mysql_db']
+app.config['MYSQL_PORT'] = cred['mysql_port']
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
 
